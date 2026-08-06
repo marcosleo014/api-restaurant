047ba2fc9395edf4067344086211596aa5dc752b
Marcos Rodrigues <marcosleo014@gmail.com>
2026-08-06 18:15:17 -0300

    Implementação do middleware de tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 
 import { routes } from "./routes";
+import { errorHandling } from "./middlewares/error-handling";
+import { AppError } from "./utils/AppError";
+
 
 const PORT = 3333;
-
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandling);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
